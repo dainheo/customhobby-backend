a1c708d50a28b0c94c1067015b93dd00025b9f4e
허다인 <jessie4497@naver.com>
2025-12-10 00:31:56 +0900

    fixing4

--- src/main/java/com/customhobby/backend/BackendApplication.java
+++ src/main/java/com/customhobby/backend/BackendApplication.java
@@ -6,12 +6,8 @@ import org.springframework.boot.autoconfigure.domain.EntityScan;
 import org.springframework.data.jpa.repository.config.EnableJpaRepositories;
 
 @SpringBootApplication
-@EntityScan(basePackages = {
-		"com.customhobby.backend.domain"   // ← 엔티티 폴더
-})
-@EnableJpaRepositories(basePackages = {
-		"com.customhobby.backend.repository"  // ← Repository 폴더
-})
+@EntityScan(basePackages = "com.customhobby.backend.domain")
+@EnableJpaRepositories(basePackages = "com.customhobby.backend.repository")
 public class BackendApplication {
 
 	public static void main(String[] args) {
